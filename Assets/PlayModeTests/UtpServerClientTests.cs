9b11c4c2d13aa6c529d772e8cf94898ef29cbfbd
dakota@wolfjawstudios.com <>
2022-03-31 16:53:22 -0400

    Created new DoTick function to make UtpServer and UtpClient connect to each other. Wrote new tests to check for connection statuses under different conditions. TODO: Check if similar solution works in UtpTransport

--- Assets/PlayModeTests/UtpServerClientTests.cs
+++ Assets/PlayModeTests/UtpServerClientTests.cs
@@ -17,6 +17,17 @@ public class UtpServerClientTests
     UtpClient _Client;
     Transport _Transport;
     int TimeoutMS = 10000;
+    IEnumerator DoTick(UtpClient client, UtpServer server) {
+        int frameCounter = 5;
+        while (frameCounter > 0)
+        {
+            client.Tick();
+            server.Tick();
+        frameCounter -= 1;
+        yield return null;
+        }
+        yield return new WaitForSeconds(1f);
+    }
     [SetUp]
     public void SetUp() {
         var Obj = new GameObject();
@@ -42,33 +53,36 @@ public class UtpServerClientTests
         _Server.Start(7777);
         Assert.IsTrue(_Server.IsActive());
     }
+    [UnityTest]
+    public IEnumerator Client_IsConnected_NotConnected_False() {
+        Assert.IsFalse(_Client.IsConnected());
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator Client_IsConnected_NoServer_False() {
+        _Client.Connect("localhost", 7777);
+        Assert.IsFalse(_Client.IsConnected());
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator Client_IsConnected_WithServer_True() {
+        _Server.Start(7777);
+        _Client.Connect("localhost", 7777);
+        yield return DoTick(_Client, _Server);
+        Assert.IsTrue(_Client.IsConnected());
+    }
     [Test]
     public void Server_GetClientAddress_NotConnected_EmptyString() {
         string clientAddress = _Server.GetClientAddress(0);
         Assert.IsEmpty(clientAddress);
     }
-    [Test]
-    public void Server_GetClientAddress_Connected_NonEmptyString() {
+    [UnityTest]
+    public IEnumerator Server_GetClientAddress_Connected_NonEmptyString() {
         _Server.Start(7777);
         _Client.Connect("localhost", 7777);
-        _Server.ProcessIncomingEvents();
+        yield return DoTick(_Client, _Server);
+        Assert.IsTrue(_Client.IsConnected());
         string clientAddress = _Server.GetClientAddress(1);
         Assert.IsNotEmpty(clientAddress);
-    }
-    [Test]
-    public void Client_IsConnected_NotConnected_False() {
-        Assert.IsFalse(_Client.IsConnected());
-    }
-    [UnityTest]
-    public IEnumerator Client_IsConnected_NoServer_False() {
-        _Client.Connect("localhost", 7777);
-        yield return new WaitForSeconds(11f);
-        Assert.IsFalse(_Client.IsConnected());
-    }
-    [Test]
-    public void Client_IsConnected_WithServer_True() {
-        _Server.Start(7777);
-        _Client.Connect("localhost", 7777);
-        Assert.IsTrue(_Client.IsConnected());
     }
 }
