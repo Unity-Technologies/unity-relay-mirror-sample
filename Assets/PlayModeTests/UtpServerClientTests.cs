7bb5e867867f716696c46b7767c1183ac9c65bd4
dakota@wolfjawstudios.com <>
2022-03-31 12:53:48 -0400

    Checking in some more tests. Just trying to figure out what's going on with getting client and server connected. Possibly need co-routines going to get them to update/tick regularly?

--- Assets/PlayModeTests/UtpServerClientTests.cs
+++ Assets/PlayModeTests/UtpServerClientTests.cs
@@ -9,51 +9,66 @@ using Utp;
 using System.Threading.Tasks;
 using Mirror;
 
-public class UtpServerTests
+public class UtpServerClientTests
 {
 
 
     UtpServer _Server;
     UtpClient _Client;
     Transport _Transport;
+    int TimeoutMS = 10000;
     [SetUp]
     public void SetUp() {
         var Obj = new GameObject();
-        _Transport = Obj.AddComponent<Transport>();
         _Server = new UtpServer(
 			(connectionId) => _Transport.OnServerConnected.Invoke(connectionId),
 			(connectionId, message) => _Transport.OnServerDataReceived.Invoke(connectionId, message, Channels.Reliable),
 			(connectionId) => _Transport.OnServerDisconnected.Invoke(connectionId),
-			1000
+			TimeoutMS
         );
         _Client = new UtpClient(
 			() => _Transport.OnClientConnected.Invoke(),
 			(message) => _Transport.OnClientDataReceived.Invoke(message, Channels.Reliable),
 			() => _Transport.OnClientDisconnected.Invoke(),
-			1000
+			TimeoutMS
         );
     }
     [Test]
-    public void IsActive_NotStarted_False() {
+    public void Server_IsActive_NotStarted_False() {
         Assert.IsFalse(_Server.IsActive());
     }
     [Test]
-    public void IsActive_Started_True() {
+    public void Server_IsActive_Started_True() {
         _Server.Start(7777);
         Assert.IsTrue(_Server.IsActive());
     }
     [Test]
-    public void GetClientAddress_NotConnected_EmptyString() {
+    public void Server_GetClientAddress_NotConnected_EmptyString() {
         string clientAddress = _Server.GetClientAddress(0);
         Assert.IsEmpty(clientAddress);
     }
-    [UnityTest]
-    public IEnumerator GetClientAddress_Connected_NonEmptyString() {
+    [Test]
+    public void Server_GetClientAddress_Connected_NonEmptyString() {
         _Server.Start(7777);
-        yield return null;
         _Client.Connect("localhost", 7777);
-        yield return new WaitForSeconds(5f);
-        string clientAddress = _Server.GetClientAddress(0);
+        _Server.ProcessIncomingEvents();
+        string clientAddress = _Server.GetClientAddress(1);
         Assert.IsNotEmpty(clientAddress);
+    }
+    [Test]
+    public void Client_IsConnected_NotConnected_False() {
+        Assert.IsFalse(_Client.IsConnected());
+    }
+    [UnityTest]
+    public IEnumerator Client_IsConnected_NoServer_False() {
+        _Client.Connect("localhost", 7777);
+        yield return new WaitForSeconds(11f);
+        Assert.IsFalse(_Client.IsConnected());
+    }
+    [Test]
+    public void Client_IsConnected_WithServer_True() {
+        _Server.Start(7777);
+        _Client.Connect("localhost", 7777);
+        Assert.IsTrue(_Client.IsConnected());
     }
 }
