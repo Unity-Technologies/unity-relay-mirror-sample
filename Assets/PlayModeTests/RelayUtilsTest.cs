367b35f41767c515f91d56af75f158832488fd4f
joe@wolfjawstudios.com <>
2022-04-11 10:25:49 -0400

    [apply-change:a1974e29] [apply-change:63ff87cd] Added parameterized testing.

--- Assets/PlayModeTests/RelayUtilsTest.cs
+++ Assets/PlayModeTests/RelayUtilsTest.cs
@@ -12,19 +12,34 @@ namespace Utp
     public class RelayUtilsTest
     {
 
+        //Test connection types
+        static RelayServerEndpoint.NetworkOptions[] connectionTypes = new RelayServerEndpoint.NetworkOptions[]
+        {
+            RelayServerEndpoint.NetworkOptions.Udp,
+            RelayServerEndpoint.NetworkOptions.Tcp
+        };
+
         #region HostRelayData
 
         /// <summary>
-        /// Tests the HostRelayData call inside RelayUtils (UDP).
+        /// Tests the HostRelayData call inside RelayUtils.
         /// </summary>
+        /// <param name="connectionType">Udp/Tcp connection option.</param>
         [Test]
-        public void HostRelayData_AllocationWithUDPEndpoints_ReturnsRelayServerData()
+        public void HostRelayData_NormalConnectionState_ReturnsRelayServerData([ValueSource("connectionTypes")]RelayServerEndpoint.NetworkOptions connectionType)
         {
+            //Get connection type as string
+            string connectionTypeString = "invalid";
+            if (connectionType == RelayServerEndpoint.NetworkOptions.Udp || connectionType == RelayServerEndpoint.NetworkOptions.Tcp)
+            {
+                connectionTypeString = connectionType == RelayServerEndpoint.NetworkOptions.Udp ? "udp" : "tcp";
+            }
+
             //Create dummy data to inject into temporary relay allocation
             RelayServer relayServer = new RelayServer("0.0.0.0", 0000);
             System.Guid allocationId = new System.Guid("00000000-0000-0000-0000-000000000000");
             List<RelayServerEndpoint> serverEndpoints = new List<RelayServerEndpoint>();
-            serverEndpoints.Add(new RelayServerEndpoint("udp", RelayServerEndpoint.NetworkOptions.Udp, false, false, "0.0.0.0", 00000));
+            serverEndpoints.Add(new RelayServerEndpoint(connectionTypeString, connectionType, false, false, "0.0.0.0", 00000));
             byte[] key = new byte[16];
             byte[] connectionData = new byte[16];
             byte[] allocationIdBytes = new byte[16];
@@ -39,66 +54,23 @@ namespace Utp
             );
 
             //Assert data against null/default data
-            RelayServerData data = RelayUtils.HostRelayData(allocation, RelayServerEndpoint.NetworkOptions.Udp);
+            RelayServerData data = RelayUtils.HostRelayData(allocation, connectionType);
             Assert.AreNotEqual(data, default(RelayServerData));
         }
 
         /// <summary>
-        /// Tests the HostRelayData call inside RelayUtils (TCP).
+        /// Tests that null allocation data throws a null exception inside HostRelayData.
         /// </summary>
+        /// <param name="connectionType">Udp/Tcp connection option.</param>
         [Test]
-        public void HostRelayData_AllocationWithTCPEndpoints_ReturnsRelayServerData()
-        {
-            //Create dummy data to inject into temporary relay allocation
-            RelayServer relayServer = new RelayServer("0.0.0.0", 0000);
-            System.Guid allocationId = new System.Guid("00000000-0000-0000-0000-000000000000");
-            List<RelayServerEndpoint> serverEndpoints = new List<RelayServerEndpoint>();
-            serverEndpoints.Add(new RelayServerEndpoint("tcp", RelayServerEndpoint.NetworkOptions.Tcp, false, false, "0.0.0.0", 00000));
-            byte[] key = new byte[16];
-            byte[] connectionData = new byte[16];
-            byte[] allocationIdBytes = new byte[16];
-
-            Allocation allocation = new Allocation(
-                allocationId,
-                serverEndpoints,
-                relayServer,
-                key,
-                connectionData,
-                allocationIdBytes
-            );
-
-            //Assert data against null/default data
-            RelayServerData data = RelayUtils.HostRelayData(allocation, RelayServerEndpoint.NetworkOptions.Tcp);
-            Assert.AreNotEqual(data, default(RelayServerData));
-        }
-
-        /// <summary>
-        /// Tests that null allocation data throws a null exception inside HostRelayData (UDP).
-        /// </summary>
-        [Test]
-        public void HostRelayData_WithNullAllocationUDP_ThrowsNullReferenceException()
+        public void HostRelayData_WithNullAllocation_ThrowsNullReferenceException([ValueSource("connectionTypes")] RelayServerEndpoint.NetworkOptions connectionType)
         {
             //Create null allocation & Assert null
             Allocation allocation = null;
 
             Assert.Throws<System.NullReferenceException>(() =>
             {
-                RelayServerData data = RelayUtils.HostRelayData(allocation, RelayServerEndpoint.NetworkOptions.Udp);
-            });
-        }
-
-        /// <summary>
-        /// Tests that null allocation data throws a null exception inside HostRelayData (TCP).
-        /// </summary>
-        [Test]
-        public void HostRelayData_WithNullAllocationTCP_ThrowsNullReferenceException()
-        {
-            //Create null allocation & Assert null
-            Allocation allocation = null;
-
-            Assert.Throws<System.NullReferenceException>(() =>
-            {
-                RelayServerData data = RelayUtils.HostRelayData(allocation, RelayServerEndpoint.NetworkOptions.Tcp);
+                RelayServerData data = RelayUtils.HostRelayData(allocation, connectionType);
             });
         }
 
@@ -112,7 +84,7 @@ namespace Utp
             RelayServer relayServer = new RelayServer("0.0.0.0", 0000);
             System.Guid allocationId = new System.Guid("00000000-0000-0000-0000-000000000000");
             List<RelayServerEndpoint> serverEndpoints = new List<RelayServerEndpoint>();
-            serverEndpoints.Add(new RelayServerEndpoint("udp", RelayServerEndpoint.NetworkOptions.Udp, false, false, "0.0.0.0", 00000));
+            serverEndpoints.Add(new RelayServerEndpoint("invalidConnType", (RelayServerEndpoint.NetworkOptions)9, false, false, "0.0.0.0", 00000));
             byte[] key = new byte[16];
             byte[] connectionData = new byte[16];
             byte[] allocationIdBytes = new byte[16];
@@ -129,15 +101,16 @@ namespace Utp
             //Assert exception thrown
             Assert.Throws<System.ArgumentException>(() =>
             {
-                RelayServerData data = RelayUtils.HostRelayData(allocation, (RelayServerEndpoint.NetworkOptions)5);
+                RelayServerData data = RelayUtils.HostRelayData(allocation, (RelayServerEndpoint.NetworkOptions)99);
             });
         }
 
         /// <summary>
         /// Tests that the endpoints list inside HostRelayData's allocation must have entries.
         /// </summary>
+        /// <param name="connectionType">Udp/Tcp connection option.</param>
         [Test]
-        public void HostRelayData_WithEmptyEndpointsInAllocation_ThrowsArgumentException()
+        public void HostRelayData_WithEmptyEndpointsInAllocation_ThrowsArgumentException([ValueSource("connectionTypes")] RelayServerEndpoint.NetworkOptions connectionType)
         {
             //Create dummy data to inject into temporary relay allocation
             RelayServer relayServer = new RelayServer("0.0.0.0", 0000);
@@ -159,7 +132,7 @@ namespace Utp
             //Assert exception thrown
             Assert.Throws<System.ArgumentException>(() =>
             {
-                RelayServerData data = RelayUtils.HostRelayData(allocation, RelayServerEndpoint.NetworkOptions.Udp);
+                RelayServerData data = RelayUtils.HostRelayData(allocation, connectionType);
             });
         }
 
@@ -168,16 +141,24 @@ namespace Utp
         #region PlayerRelayData
 
         /// <summary>
-        /// Tests the PlayerRelayData call inside RelayUtils (UDP).
+        /// Tests the PlayerRelayData call inside RelayUtils.
         /// </summary>
-        [UnityTest]
-        public void PlayerRelayData_AllocationWithUDPEndpoints_ReturnsRelayServerData()
+        /// <param name="connectionType">Udp/Tcp connection option.</param>
+        [Test]
+        public void PlayerRelayData_NormalConnectionState_ReturnsRelayServerData([ValueSource("connectionTypes")] RelayServerEndpoint.NetworkOptions connectionType)
         {
+            //Get connection type as string
+            string connectionTypeString = "invalid";
+            if (connectionType == RelayServerEndpoint.NetworkOptions.Udp || connectionType == RelayServerEndpoint.NetworkOptions.Tcp)
+            {
+                connectionTypeString = connectionType == RelayServerEndpoint.NetworkOptions.Udp ? "udp" : "tcp";
+            }
+
             //Create dummy data to inject into temporary relay allocation
             RelayServer relayServer = new RelayServer("0.0.0.0", 0000);
             System.Guid allocationId = new System.Guid("00000000-0000-0000-0000-000000000000");
             List<RelayServerEndpoint> serverEndpoints = new List<RelayServerEndpoint>();
-            serverEndpoints.Add(new RelayServerEndpoint("udp", RelayServerEndpoint.NetworkOptions.Udp, false, false, "0.0.0.0", 00000));
+            serverEndpoints.Add(new RelayServerEndpoint(connectionTypeString, connectionType, false, false, "0.0.0.0", 00000));
             byte[] key = new byte[16];
             byte[] connectionData = new byte[16];
             byte[] allocationIdBytes = new byte[16];
@@ -194,68 +175,23 @@ namespace Utp
             );
 
             //Assert data against null/default data
-            RelayServerData data = RelayUtils.PlayerRelayData(allocation, RelayServerEndpoint.NetworkOptions.Udp);
+            RelayServerData data = RelayUtils.PlayerRelayData(allocation, connectionType);
             Assert.AreNotEqual(data, default(RelayServerData));
         }
 
         /// <summary>
-        /// Tests the PlayerRelayData call inside RelayUtils (TCP).
+        /// Tests that null allocation data throws a null exception inside PlayerRelayData.
         /// </summary>
-        [UnityTest]
-        public void PlayerRelayData_AllocationWithTCPEndpoints_ReturnsRelayServerData()
-        {
-            //Create dummy data to inject into temporary relay allocation
-            RelayServer relayServer = new RelayServer("0.0.0.0", 0000);
-            System.Guid allocationId = new System.Guid("00000000-0000-0000-0000-000000000000");
-            List<RelayServerEndpoint> serverEndpoints = new List<RelayServerEndpoint>();
-            serverEndpoints.Add(new RelayServerEndpoint("tcp", RelayServerEndpoint.NetworkOptions.Tcp, false, false, "0.0.0.0", 00000));
-            byte[] key = new byte[16];
-            byte[] connectionData = new byte[16];
-            byte[] allocationIdBytes = new byte[16];
-            byte[] hostConnectionData = new byte[16];
-
-            JoinAllocation allocation = new JoinAllocation(
-                allocationId,
-                serverEndpoints,
-                relayServer,
-                key,
-                connectionData,
-                allocationIdBytes,
-                hostConnectionData
-            );
-
-            //Assert data against null/default data
-            RelayServerData data = RelayUtils.PlayerRelayData(allocation, RelayServerEndpoint.NetworkOptions.Tcp);
-            Assert.AreNotEqual(data, default(RelayServerData));
-        }
-
-        /// <summary>
-        /// Tests that null allocation data throws a null exception inside PlayerRelayData (UDP).
-        /// </summary>
+        /// <param name="connectionType">Udp/Tcp connection option.</param>
         [Test]
-        public void PlayerRelayData_WithNullAllocationUDP_ThrowsNullReferenceException()
+        public void PlayerRelayData_WithNullAllocation_ThrowsNullReferenceException([ValueSource("connectionTypes")] RelayServerEndpoint.NetworkOptions connectionType)
         {
             //Create null allocation & Assert null
             JoinAllocation allocation = null;
 
             Assert.Throws<System.NullReferenceException>(() =>
             {
-                RelayServerData data = RelayUtils.PlayerRelayData(allocation, RelayServerEndpoint.NetworkOptions.Udp);
-            });
-        }
-
-        /// <summary>
-        /// Tests that null allocation data throws a null exception inside PlayerRelayData (TCP).
-        /// </summary>
-        [Test]
-        public void PlayerRelayData_WithNullAllocationTCP_ThrowsNullReferenceException()
-        {
-            //Create null allocation & Assert null
-            JoinAllocation allocation = null;
-
-            Assert.Throws<System.NullReferenceException>(() =>
-            {
-                RelayServerData data = RelayUtils.PlayerRelayData(allocation, RelayServerEndpoint.NetworkOptions.Tcp);
+                RelayServerData data = RelayUtils.PlayerRelayData(allocation, connectionType);
             });
         }
 
@@ -269,7 +205,7 @@ namespace Utp
             RelayServer relayServer = new RelayServer("0.0.0.0", 0000);
             System.Guid allocationId = new System.Guid("00000000-0000-0000-0000-000000000000");
             List<RelayServerEndpoint> serverEndpoints = new List<RelayServerEndpoint>();
-            serverEndpoints.Add(new RelayServerEndpoint("udp", RelayServerEndpoint.NetworkOptions.Udp, false, false, "0.0.0.0", 00000));
+            serverEndpoints.Add(new RelayServerEndpoint("invalidConnType", (RelayServerEndpoint.NetworkOptions)99, false, false, "0.0.0.0", 00000));
             byte[] key = new byte[16];
             byte[] connectionData = new byte[16];
             byte[] allocationIdBytes = new byte[16];
@@ -288,15 +224,16 @@ namespace Utp
             //Assert exception thrown
             Assert.Throws<System.ArgumentException>(() =>
             {
-                RelayServerData data = RelayUtils.PlayerRelayData(allocation, (RelayServerEndpoint.NetworkOptions)5);
+                RelayServerData data = RelayUtils.PlayerRelayData(allocation, (RelayServerEndpoint.NetworkOptions)99);
             });
         }
 
         /// <summary>
         /// Tests that the endpoints list inside PlayerRelayData's allocation must have entries.
         /// </summary>
+        /// <param name="connectionType">Udp/Tcp connection option.</param>
         [Test]
-        public void PlayerRelayData_WithEmptyEndpointsInAllocation_ThrowsArgumentException()
+        public void PlayerRelayData_WithEmptyEndpointsInAllocation_ThrowsArgumentException([ValueSource("connectionTypes")] RelayServerEndpoint.NetworkOptions connectionType)
         {
             //Create dummy data to inject into temporary relay allocation
             RelayServer relayServer = new RelayServer("0.0.0.0", 0000);
@@ -320,7 +257,7 @@ namespace Utp
             //Assert exception thrown
             Assert.Throws<System.ArgumentException>(() =>
             {
-                RelayServerData data = RelayUtils.PlayerRelayData(allocation, RelayServerEndpoint.NetworkOptions.Udp);
+                RelayServerData data = RelayUtils.PlayerRelayData(allocation, connectionType);
             });
         }
 
