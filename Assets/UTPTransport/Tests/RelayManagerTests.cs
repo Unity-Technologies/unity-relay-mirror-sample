8bb909668b790c1c352f27f74220af8d3dc2ae5f
james@wolfjawstudios.com <james@jamesmarcil.com>
2022-04-15 11:04:52 -0400

    Cleanup using statements in RelayManagerTests.

--- Assets/UTPTransport/Tests/RelayManagerTests.cs
+++ Assets/UTPTransport/Tests/RelayManagerTests.cs
@@ -1,10 +1,7 @@
 using NUnit.Framework;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.TestTools;
 using Unity.Services.Relay.Models;
-using Utp;
+using UnityEngine;
 
 namespace Utp
 {
