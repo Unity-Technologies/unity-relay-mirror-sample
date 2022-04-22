37600251b5eea81f95abbfec338c3e2ad5aed6d7
james@wolfjawstudios.com <james@jamesmarcil.com>
2022-04-22 11:14:44 -0400

    Adjusted the whitespace in UtpTransportTests.

--- Assets/UTPTransport/Tests/UtpTransportTests.cs
+++ Assets/UTPTransport/Tests/UtpTransportTests.cs
@@ -7,8 +7,6 @@ namespace Utp
 {
     public class UtpTransportTests
     {
-
-
         private UtpTransport _server;
         private UtpTransport _client;
 
@@ -21,6 +19,7 @@ namespace Utp
             var ClientObj = new GameObject();
             _client = ClientObj.AddComponent<UtpTransport>();
         }
+
         [TearDown]
         public void TearDown()
         {
@@ -30,17 +29,20 @@ namespace Utp
             _server.ServerStop();
             GameObject.Destroy(_server.gameObject);
         }
+
         [Test]
         public void ServerActive_IsNotActive_False()
         {
             Assert.IsFalse(_server.ServerActive(), "Server is running, but should not be.");
         }
+
         [Test]
         public void ServerActive_IsActive_True()
         {
             _server.ServerStart();
             Assert.IsTrue(_server.ServerActive(), "Server is not running, but should be.");
         }
+
         [Test]
         public void ServerStop_IsActive_False()
         {
@@ -48,12 +50,14 @@ namespace Utp
             _server.ServerStop();
             Assert.IsFalse(_server.ServerActive(), "Server is running, but should not be.");
         }
+
         [Test]
         public void ServerGetClientAddress_InvalidAddress_EmptyString()
         {
             string clientAddress = _server.ServerGetClientAddress(0);
             Assert.IsEmpty(clientAddress, "A client address was returned instead of an empty string.");
         }
+
         [UnityTest]
         public IEnumerator ServerGetClientAddress_clientConnected_NonEmptyString()
         {
@@ -64,11 +68,13 @@ namespace Utp
             string clientAddress = _server.ServerGetClientAddress(idOfFirstClient);
             Assert.IsNotEmpty(clientAddress, "A client address was not returned, connection possibly timed out..");
         }
+
         [Test]
         public void ClientConnected_NotConnected_False()
         {
             Assert.IsFalse(_client.ClientConnected(), "Client is connected, but should not be.");
         }
+
         [UnityTest]
         public IEnumerator ClientConnected_IsConnected_True()
         {
