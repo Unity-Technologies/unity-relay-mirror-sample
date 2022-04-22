91c86cc79cd553de1864d4f52c718b097693fde5
james@wolfjawstudios.com <james@jamesmarcil.com>
2022-04-22 13:40:52 -0400

    Reworked the tests in UtpServerTests.

--- Assets/UTPTransport/Tests/UtpServerTests.cs
+++ Assets/UTPTransport/Tests/UtpServerTests.cs
@@ -13,19 +13,16 @@ namespace Utp
         private bool _serverOnConnectedCalled;
         private bool _serverOnDisconnectedCalled;
         private bool _serverOnReceivedDataCalled;
-        private bool _clientOnConnectedCalled;
-        private bool _clientOnDisconnectedCalled;
-        private bool _clientOnReceivedDataCalled;
 
-        public IEnumerator TickFrames(UtpClient _Client, UtpServer _Server, int FramesToSkip = 15)
+        private IEnumerator tickClientAndServerForSeconds(UtpClient client, UtpServer server, float numSeconds)
         {
-            int FramesPassed = 0;
-            while (FramesPassed < FramesToSkip)
+            float elapsedTime = 0f;
+            while (elapsedTime < numSeconds)
             {
-                _Client.Tick();
-                _Server.Tick();
+                client.Tick();
+                server.Tick();
                 yield return null;
-                FramesPassed++;
+                elapsedTime += Time.deltaTime;
             }
         }
 
@@ -41,9 +38,9 @@ namespace Utp
             );
 
             _client = new UtpClient(
-                () => { _clientOnConnectedCalled = true; },
-                (message) => { _clientOnReceivedDataCalled = true; },
-                () => { _clientOnDisconnectedCalled = true; },
+                () => { },
+                (message) => { },
+                () => { },
                 timeout: 1000
             );
         }
@@ -53,124 +50,133 @@ namespace Utp
         {
             _client.Disconnect();
             _server.Stop();
+
             _serverOnConnectedCalled = false;
             _serverOnDisconnectedCalled = false;
             _serverOnReceivedDataCalled = false;
-            _clientOnConnectedCalled = false;
-            _clientOnDisconnectedCalled = false;
-            _clientOnReceivedDataCalled = false;
         }
 
         [Test]
-        public void IsActive_NotStarted_False()
+        public void IsActive_ServerWasNotStarted_ReturnsFalse()
         {
             Assert.IsFalse(_server.IsActive(), "Server is active without being started.");
         }
 
         [Test]
-        public void IsActive_Started_True()
+        public void IsActive_ServerWasStarted_ReturnsTrue()
         {
             _server.Start(7777);
+
             Assert.IsTrue(_server.IsActive(), "Server did not start and is not active.");
         }
 
         [Test]
-        public void GetClientAddress_NotConnected_EmptyString()
+        public void GetClientAddress_ConnectionIdOfNonExistentClient_ReturnsEmptyString()
         {
-            int idOfNonExistentClient = 1;
-            string clientAddress = _server.GetClientAddress(idOfNonExistentClient);
+            int connectionIdOfNonExistentClient = 1;
+
+            string clientAddress = _server.GetClientAddress(connectionIdOfNonExistentClient);
+
             Assert.IsEmpty(clientAddress, "Client address was not empty.");
         }
 
         [UnityTest]
-        public IEnumerator GetClientAddress_Connected_NonEmptyString()
+        public IEnumerator GetClientAddress_ConnectionIdOfConnectedClient_ReturnsClientAddress()
         {
+            int idOfConnectedClient = 1;
             _server.Start(7777);
             _client.Connect("localhost", 7777);
-
             yield return new WaitForClientAndServerToConnect(client: _client, server: _server, timeoutInSeconds: 30f);
 
-            int idOfFirstClient = 1;
-            string clientAddress = _server.GetClientAddress(idOfFirstClient);
+            string clientAddress = _server.GetClientAddress(idOfConnectedClient);
+
             Assert.IsNotEmpty(clientAddress, "Client address was empty, indicating the client is probably not connected.");
         }
 
         [Test]
-        public void Disconnect_NoClient_Warning()
+        public void Disconnect_ConnectionIdOfNonExistentClient_LogsWarning()
         {
-            int idOfNonExistentClient = 1;
-            _server.Disconnect(idOfNonExistentClient);
-            LogAssert.Expect(LogType.Warning, "Connection not found: 1");
+            int connectionIdOfNonExistentClient = 1;
 
+            _server.Disconnect(connectionId: connectionIdOfNonExistentClient);
+
+            LogAssert.Expect(LogType.Warning, "Connection not found: 1");
         }
 
         [UnityTest]
-        public IEnumerator Disconnect_ClientConnected_Success()
+        public IEnumerator Disconnect_ConnectionIdOfConnectedClient_ClientIsDisconnected()
         {
+            int connectionIdOfConnectedClient = 1;
             _server.Start(7777);
             _client.Connect("localhost", 7777);
             yield return new WaitForClientAndServerToConnect(client: _client, server: _server, timeoutInSeconds: 30f);
-            Assert.IsTrue(_client.IsConnected(), "Client did not connect to server.");
-            int idOfFirstClient = 1;
-            _server.Disconnect(idOfFirstClient);
+
+            _server.Disconnect(connectionId: connectionIdOfConnectedClient);
             yield return new WaitForClientAndServerToDisconnect(client: _client, server: _server, timeoutInSeconds: 10f);
+            
             Assert.IsFalse(_client.IsConnected(), "Client was not successfully disconnected from server");
         }
 
         [Test]
-        public void FindConnection_NoClient_DefaultConnection()
+        public void FindConnection_ConnectionIdOfNonExistentClient_ReturnsDefaultNetworkConnection()
         {
-            int idOfNonExistentClient = 1;
-            Unity.Networking.Transport.NetworkConnection FoundConnection = _server.FindConnection(idOfNonExistentClient);
-            Assert.IsTrue(FoundConnection == default(Unity.Networking.Transport.NetworkConnection), "A connection was found when no client was connected.");
+            int connectionIdOfNonExistentClient = 1;
+
+            Unity.Networking.Transport.NetworkConnection connection = _server.FindConnection(connectionIdOfNonExistentClient);
+
+            Assert.That(connection, Is.EqualTo(default(Unity.Networking.Transport.NetworkConnection)));
         }
 
         [UnityTest]
-        public IEnumerator FindConnection_ClientConnected_ValidConnection()
+        public IEnumerator FindConnection_ConnectionIdOfConnectedClient_ReturnsNetworkConnection()
         {
-            _server.Start(7777);
-            _client.Connect("localhost", 7777);
-            int idOfFirstClient = 1;
-            yield return new WaitForClientAndServerToConnect(client: _client, server: _server, timeoutInSeconds: 30f);
-            Unity.Networking.Transport.NetworkConnection FoundConnection = _server.FindConnection(idOfFirstClient);
-            Assert.IsFalse(FoundConnection == default(Unity.Networking.Transport.NetworkConnection), "No connection found.");
-        }
-
-        [UnityTest]
-        public IEnumerator OnConnectedCallback_Called()
-        {
+            int connectionIdOfConnectedClient = 1;
             _server.Start(7777);
             _client.Connect("localhost", 7777);
             yield return new WaitForClientAndServerToConnect(client: _client, server: _server, timeoutInSeconds: 30f);
-            Assert.IsTrue(_serverOnConnectedCalled, "The Server.OnConnected callback was not invoked as expected.");
+
+            Unity.Networking.Transport.NetworkConnection connection = _server.FindConnection(connectionIdOfConnectedClient);
+
+            Assert.That(connection, Is.Not.EqualTo(default(Unity.Networking.Transport.NetworkConnection)));
         }
 
         [UnityTest]
-        public IEnumerator OnDisconnectedCallback_Called()
+        public IEnumerator OnConnected_ClientConnectsToServer_CallbackIsInvoked()
         {
             _server.Start(7777);
             _client.Connect("localhost", 7777);
             yield return new WaitForClientAndServerToConnect(client: _client, server: _server, timeoutInSeconds: 30f);
-            yield return TickFrames(_client, _server, 5);
-            int idOfFirstClient = 1;
-            _server.Disconnect(idOfFirstClient);
+
+            Assert.That(_serverOnConnectedCalled, Is.True);
+        }
+
+        [UnityTest]
+        public IEnumerator OnDisconnect_ServerDisconnectsClient_CallbackIsInvoked()
+        {
+            int idOfConnectedClient = 1;
+            _server.Start(7777);
+            _client.Connect("localhost", 7777);
+            yield return new WaitForClientAndServerToConnect(client: _client, server: _server, timeoutInSeconds: 30f);
+
+            _server.Disconnect(idOfConnectedClient);
             yield return new WaitForClientAndServerToDisconnect(client: _client, server: _server, timeoutInSeconds: 30f);
-            Assert.IsTrue(_clientOnDisconnectedCalled, "The UtpClient.OnDisconnected callback was not invoked as expected.");
+
+            Assert.That(_serverOnDisconnectedCalled, Is.True);
         }
 
         [UnityTest]
-        public IEnumerator OnReceivedDataCallback_Called()
+        public IEnumerator OnReceivedData_ClientSendsDataToServer_CallbackIsInvoked()
         {
             _server.Start(7777);
             _client.Connect("localhost", 7777);
             yield return new WaitForClientAndServerToConnect(client: _client, server: _server, timeoutInSeconds: 30f);
-            int idOfFirstClient = 1;
-            int idOfChannel = 1;
-            ArraySegment<byte> emptyPacket = new ArraySegment<byte>(new byte[4]);
-            _client.Send(emptyPacket, idOfChannel);
-            _server.Send(idOfFirstClient, emptyPacket, idOfChannel);
-            yield return TickFrames(_client, _server, 5);
-            Assert.IsTrue(_serverOnReceivedDataCalled, "The Server.OnReceivedData callback was not invoked as expected.");
+
+            int validChannelId = 1;
+            var dummyDataToSend = new ArraySegment<byte>(new byte[4]);
+            _client.Send(segment: dummyDataToSend, channelId: validChannelId);
+            yield return tickClientAndServerForSeconds(client: _client, server: _server, numSeconds: 5f);
+
+            Assert.That(_serverOnReceivedDataCalled, Is.True);
         }
     }
 }
