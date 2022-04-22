80ff910885dfff567b7eb15c0ed98d7de45ccf84
james@wolfjawstudios.com <james@jamesmarcil.com>
2022-04-22 12:34:12 -0400

    Reworked the tests in UtpTransportTests.

--- Assets/UTPTransport/Tests/UtpTransportTests.cs
+++ Assets/UTPTransport/Tests/UtpTransportTests.cs
@@ -31,56 +31,65 @@ namespace Utp
         }
 
         [Test]
-        public void ServerActive_IsNotActive_False()
+        public void ServerActive_ServerIsNotActive_ReturnsFalse()
         {
             Assert.IsFalse(_server.ServerActive(), "Server is running, but should not be.");
         }
 
         [Test]
-        public void ServerActive_IsActive_True()
+        public void ServerActive_ServerIsActive_ReturnsTrue()
         {
             _server.ServerStart();
+
             Assert.IsTrue(_server.ServerActive(), "Server is not running, but should be.");
         }
 
         [Test]
-        public void ServerStop_IsActive_False()
+        public void ServerStop_ServerIsActive_ServerIsNoLongerActive()
         {
             _server.ServerStart();
+
             _server.ServerStop();
+
             Assert.IsFalse(_server.ServerActive(), "Server is running, but should not be.");
         }
 
         [Test]
-        public void ServerGetClientAddress_InvalidAddress_EmptyString()
+        public void ServerGetClientAddress_ConnectionIdOfNonExistentClient_ReturnsEmptyString()
         {
-            string clientAddress = _server.ServerGetClientAddress(0);
+            int connectionIdForNonExistentClient = 1;
+
+            string clientAddress = _server.ServerGetClientAddress(connectionId: connectionIdForNonExistentClient);
+
             Assert.IsEmpty(clientAddress, "A client address was returned instead of an empty string.");
         }
 
         [UnityTest]
-        public IEnumerator ServerGetClientAddress_clientConnected_NonEmptyString()
+        public IEnumerator ServerGetClientAddress_ConnectionIdOfConnectedClient_ReturnsIpAddress()
         {
+            int connectionIdForConnectedClient = 1;
             _server.ServerStart();
             _client.ClientConnect(_server.ServerUri());
-            yield return new WaitForTransportToConnect(_client, _server, 30f);
-            int idOfFirstClient = 1;
-            string clientAddress = _server.ServerGetClientAddress(idOfFirstClient);
+            yield return new WaitForTransportToConnect(client: _client, server: _server, timeoutInSeconds: 30f);
+
+            string clientAddress = _server.ServerGetClientAddress(connectionId: connectionIdForConnectedClient);
+
             Assert.IsNotEmpty(clientAddress, "A client address was not returned, connection possibly timed out..");
         }
 
         [Test]
-        public void ClientConnected_NotConnected_False()
+        public void ClientConnected_ClientIsNotConnectedToServer_ReturnsFalse()
         {
             Assert.IsFalse(_client.ClientConnected(), "Client is connected, but should not be.");
         }
 
         [UnityTest]
-        public IEnumerator ClientConnected_IsConnected_True()
+        public IEnumerator ClientConnected_ClientIsConnectedToServer_ReturnsTrue()
         {
             _server.ServerStart();
             _client.ClientConnect(_server.ServerUri());
-            yield return new WaitForTransportToConnect(_client, _server, 30f);
+            yield return new WaitForTransportToConnect(client: _client, server: _server, timeoutInSeconds: 30f);
+
             Assert.IsTrue(_client.ClientConnected(), "Client is not connected, but should be.");
         }
     }
